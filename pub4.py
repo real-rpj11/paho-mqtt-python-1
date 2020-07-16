28c49059b3e181c575367c74c85c3529fc4f956a
junreycloudstrife <junrey.cloudstrife@gmail.com>
2020-07-16 08:16:02 +0800

    Implemtend samples for topics with wildcard

---
+++ pub4.py
@@ -0,0 +1,24 @@
+import paho.mqtt.client as mqtt
+import time
+
+topic = "junrey/pn/c2/comlab/humidity"
+payload = "75%"
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    print(" ")
+    print("This is a publisher client")
+    print("Published topic: " + topic)
+    print("Published payload: " + payload)
+    print(" ")
+
+client = mqtt.Client()
+client.on_connect = on_connect
+
+client.connect("mqtt.eclipse.org", 1883, 60)
+
+time.sleep(1)
+while True:
+    client.loop()
+    client.publish(topic, payload)
+    time.sleep(1)
